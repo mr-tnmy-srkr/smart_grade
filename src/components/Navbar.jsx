aad2dfaacb4af899c620c5fefabc337fb6c8f425
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-12 20:41:14 +0530

    navbar and banner comp added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/lws-logo-en.svg"
+
+const Navbar = () => {
+  return (
+    <nav className="py-6">
+      <div className="container flex items-center justify-between gap-x-6">
+        {/* <!-- Logo --> */}
+        <a href="/">
+          <img className="h-[40px]" src={logo} alt="Lws" />
+        </a>
+        {/* <!-- Logo Ends --> */}
+        <a className="px-5 py-2 bg-[#172227] rounded-[44px]" href="#">
+          Get Admission
+        </a>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
